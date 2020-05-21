59b0eb6243ad755c1d21be37e38762dcda92f2a3
Josewp10 <jdmowp10@gmail.com>
2020-05-20 23:39:22 -0500

    Despliegue heroku

--- app.js
+++ app.js
@@ -1,42 +1,41 @@
 //Importar express
-const express = require('express')
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 //Inicializar librería
-const app = express()
+const app = express();
 app.use(express.json());
 app.use(cors());
 
 //Endpoint
-app.get("/", (req, res)=>{
-  res.send('Bienvenido al proyecto de gestión de tareas')
-})
+app.get("/", (req, res) => {
+  res.send("Bienvenido al proyecto de gestión de tareas");
+});
 const vs = "/api/v1/";
 ///Importarlas rutas
-const ruta_autenticacion= require("./routes/autenticacion")
+const ruta_autenticacion = require("./routes/autenticacion");
 app.use(vs, ruta_autenticacion);
 
-const ruta_reporte = require("./routes/reportes")
+const ruta_reporte = require("./routes/reportes");
 app.use(ruta_reporte);
 
-const ruta_reporteConsultado = require("./routes/reporteConsulta")
+const ruta_reporteConsultado = require("./routes/reporteConsulta");
 app.use(ruta_reporteConsultado);
 
-const ruta_obra = require("./routes/obra")
+const ruta_obra = require("./routes/obra");
 app.use(ruta_obra);
 
-const ruta_seguimiento = require("./routes/seguimiento")
+const ruta_seguimiento = require("./routes/seguimiento");
 app.use(ruta_seguimiento);
 
-const ruta_tareas = require("./routes/tareas")
+const ruta_tareas = require("./routes/tareas");
 app.use(ruta_tareas);
 
-const ruta_envioCorreo = require("./routes/envioCorreo")
+const ruta_envioCorreo = require("./routes/envioCorreo");
 app.use(ruta_envioCorreo);
 
-
 //Puerto
-const port = 3001
+const port = process.env.PORT || 3001;
 //Levantamiento
-app.listen(port,()=>{
+app.listen(port, () => {
   console.log(`Escuchando API en http://localhost:${port}`);
-})
+});
